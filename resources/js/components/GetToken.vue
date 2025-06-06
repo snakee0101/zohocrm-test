07d5dec6cfda52091ac25167ac971ba77cb2cf3d
snakee0101 <snakee0101@gmail.com>
2025-06-06 18:37:52 +0300

    add get token button

--- resources/js/components/GetToken.vue
+++ resources/js/components/GetToken.vue
@@ -20,6 +20,17 @@
         'code': props.user.user.api_token ? props.user.user.api_token.authorization_code : null
     });
 
+    const params = new URLSearchParams({
+           response_type: 'code',
+           client_id: '1000.7DBABT5NN8EJYMLJABQVIMHIUGHIGT',
+           redirect_uri: 'http://127.0.0.1:8000/oauthredirect',
+           scope: 'ZohoCRM.modules.ALL',
+           access_type: 'offline',
+           prompt: 'consent'
+   });
+
+   const authUrl = `https://accounts.zoho.com/oauth/v2/auth?${params.toString()}`;
+
     function saveToken() {
         axios.post('/access_token', {
             apiResponse: apiResponse.value
@@ -31,6 +42,14 @@
 
 <template>
     <div class="mb-8 space-y-0.5">
+         <a :href="authUrl"
+            class="bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800 transition"
+         >
+           <button class="bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800 transition" @click="getToken">
+               1. Get Authorization Code
+           </button>
+       </a>
+
         <p class="mb-2">
             <form :action="`http://accounts.zoho.eu/oauth/v2/token?${accessTokenURLParams.toString()}`" method="post">
                 <button class="bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800 transition">
